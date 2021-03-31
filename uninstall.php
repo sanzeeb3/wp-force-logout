0ff2ca1844179d1fc7636b500622275978652a53
Sanzeeb Aryal <sanzeeb.aryal@gmail.com>
2021-03-31 13:15:20 +0545

    Add uninstall.php

---
+++ uninstall.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Uninstall WPForce Logout.
+ *
+ * @since 1.4.5
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}
+
+global $wpdb;
+
+// Delete option set for the review notice.
+delete_option( 'wpfl_review_notice_dismissed' );
+
+// Delete online status stored in transient.
+delete_transient( 'online_status' );
+
+// Delete all users last_login meta key.
+$wpdb->query( "DELETE FROM $wpdb->usermeta WHERE meta_key = 'last_login';" );
