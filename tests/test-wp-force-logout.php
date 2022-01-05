a64d7f886b8f009e43c7dee9f298007dd6af6276
Sanzeeb Aryal <sanzeeb.aryal@gmail.com>
2022-01-05 14:45:31 +0545

    Write test for last login date

--- tests/test-wp-force-logout.php
+++ tests/test-wp-force-logout.php
@@ -21,8 +21,17 @@ class WP_Force_Logout_Test extends WP_UnitTestCase {
 	public function test_is_user_online() {
 
 		$result   = $this->class_instance->is_user_online( 'Invalid User ID passed.' );
-		$expected = false;
+		$this->assertEquals( false, $result );
+	}
 
-		$this->assertEquals( $expected, $result );
+	/**
+	 * Test when invalid user ID is passed to get_last_login.
+	 *
+	 * @since 1.5.0
+	 */
+	public function test_get_last_login() {
+		$result = $this->class_instance->get_last_login( 'invalid User ID passed.' );
+
+		$this->assertequals( '', $result );
 	}
 }
